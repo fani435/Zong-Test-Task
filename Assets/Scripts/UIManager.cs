b98ceb317c27fefa08887e1191151b0f0eb135b6
fani435 <farhansarwar435@gmail.com>
2022-11-04 00:14:18 +0500

    UI script and boxes added

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public Button previousButton;
+    public Color greenColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ButtonPressed(Button pressedButton)
+    {
+        previousButton.image.color = greenColor;
+        pressedButton.image.color = Color.white;
+        previousButton = pressedButton;
+    }
+}
